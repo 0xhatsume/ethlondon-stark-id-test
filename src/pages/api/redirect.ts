e67264fb8223016c8d595b51c22b0639e5a76145
0xhatsume <0xhatsume@gmail.com>
2024-03-16 23:31:42 +0000

    :pencil2: Fix typo for redirect

--- src/pages/api/redirect.ts
+++ src/pages/api/redirect.ts
@@ -22,7 +22,7 @@ export default async function handler(
     };
 
     const messageBytes = signedMessage?.trustedData?.messageBytes;
-    res.redirect(302, `${BASE_URL}verify/${messageBytes}`);
+    res.redirect(302, `${BASE_URL}/verify/${messageBytes}`);
   } else {
     res.status(405).end(); // Method Not Allowed
   }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -16,7 +16,7 @@ export default function Home() {
           content={`${BASE_URL}/api/redirect`}
         />
       </Head>
-      Henlos Bro wif Local Storage
+      Henlos Bro wif Local Storage v3
     </>
   );
 }
