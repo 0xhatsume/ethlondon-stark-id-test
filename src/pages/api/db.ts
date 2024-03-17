ad837825c56e3c99a747662b6826e4229b5e86a4
0xhatsume <0xhatsume@gmail.com>
2024-03-17 01:02:32 +0000

    :loud_sound: Add logs for db operations

--- src/pages/api/db.ts
+++ src/pages/api/db.ts
@@ -18,11 +18,13 @@ export async function addOrUpdateMapping(
         "UPDATE public.user_mappings SET starknet_address = $1 WHERE fid = $2",
         [starknetAddress, fid]
       );
+      console.log("updating mapping fid: " + fid + " starknetAddress: " + starknetAddress)
     } else {
       await client.query(
         "INSERT INTO public.user_mappings (fid, starknet_address) VALUES ($1, $2)",
         [fid, starknetAddress]
       );
+      console.log("inserting new mapping fid: " + fid + " starknetAddress: " + starknetAddress)
     }
   } finally {
     client.release();
@@ -37,6 +39,7 @@ export async function getMappingByFid(fid: number): Promise<string | null> {
       "SELECT starknet_address FROM public.user_mappings WHERE fid = $1",
       [fid]
     );
+    console.log("trying to get mapping of fid: ", fid)
     return res.rows[0]?.starknet_address || null;
   } finally {
     client.release();
@@ -53,6 +56,7 @@ export async function getMappingByStarknetAddress(
       "SELECT fid FROM public.user_mappings WHERE starknet_address = $1",
       [starknetAddress]
     );
+    console.log("trying to get mapping of starknet address: ", starknetAddress)
     return res.rows[0]?.fid || null;
   } finally {
     client.release();
